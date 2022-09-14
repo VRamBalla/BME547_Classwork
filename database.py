de9672ba9e33d45a9aa7b8ba09dbd2253083ca8f
Ramana Balla <vrb12@duke.edu>
2022-09-14 13:19:43 -0400

    edited the database file to make db, add patients, search patients

--- database.py
+++ database.py
@@ -1,11 +1,39 @@
-# database.py
-print("This is the database.py module.")
-print("Python thinks this is called {}".format(__name__))
+def create_patient_entry(patient_name, patient_id, patient_age):
+    new_patient = [patient_name, patient_id, patient_age, []]
+    return new_patient
 
-from blood_calculator import *
+def output(db):
+    for x in db:
+        print(x)
+        print("Name: {}, ID: {}, Age: {} \n".format(x[0],x[1],x[2]))
 
-answer = check_HDL(55)
-print("The HDL of 55 is {}".format(answer))
+def search_db(db,id):
+    for x in db:
+        if x[1] == id:
+            return x
+    return False
 
-answer = check_LDL(55)
-
+def add_test_results(db,id,test_name,test_value):
+    patient = search_db(db,id)
+    patient[3].append([test_name,test_value])
+    
+   
+def main():
+    db = []
+    db.append(create_patient_entry("Ann Ables", 1, 30))
+    db.append(create_patient_entry("Bob Boyles", 2, 34))
+    db.append(create_patient_entry("Chris Chou", 3, 25))
+    add_test_results(db,2,"HDL",100)
+    print(search_db(db,2))
+    
+    room_list = ["Room 1", "Room 2", "Room 3"]
+    
+    for i, patient in enumerate(db):
+        print(i)
+        print("Name = {}, Room = {}".format(patient[0],room_list[i]))
+    
+    for patient, room in zip(db,room_list):
+        print("Name = {}, Room = {}".format(patient[0],room))
+  
+if __name__ == "__main__":
+    main()
