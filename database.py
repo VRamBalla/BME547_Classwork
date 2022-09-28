d4d966126fa6247485f82b89f18da2125b3c3615
Ramana Balla <vrb12@duke.edu>
2022-09-27 23:57:20 -0400

    converted patient db to dictionary

--- database.py
+++ database.py
@@ -1,39 +1,56 @@
-def create_patient_entry(patient_name, patient_id, patient_age):
-    new_patient = [patient_name, patient_id, patient_age, []]
+def create_patient_entry(patient_first_name, 
+                         patient_last_name,
+                         patient_id, 
+                         patient_age):
+    new_patient = {"First Name": patient_first_name,
+                   "Last Name": patient_last_name,
+                   "ID": patient_id, 
+                   "Age": patient_age,
+                   "Tests": []}
     return new_patient
 
 
 def output(db):
-    for x in db:
-        print(x)
-        print("Name: {}, ID: {}, Age: {} \n".format(x[0], x[1], x[2]))
+    for patient in db:
+        print(patient)
+        print("Name: {}, ID: {}, Age: {} \n".format(get_full_name(patient), 
+                                                    patient["ID"], 
+                                                    patient["Age"]))
 
 
-def search_db(db, id):
+def get_full_name(patient):
+    full_name = "{} {}".format(patient["First Name"], patient["Last Name"])
+    return full_name
+
+
+def search_db(db, id_no):
     for x in db:
-        if x[1] == id:
+        if x["ID"] == id_no:
             return x
     return False
 
 
-def add_test_results(db, id, test_name, test_value):
-    patient = search_db(db, id)
-    patient[3].append([test_name, test_value])
+def add_test_results(db, id_no, test_name, test_value):
+    patient = search_db(db, id_no)
+    patient["Tests"].append([test_name, test_value])
+
+def adult_or_minor(patient):
+    if patient["Age"] >= 18:
+        return "adult"
+    else:
+        return "minor"
 
 
 def main():
     db = []
-    db.append(create_patient_entry("Ann Ables", 1, 30))
-    db.append(create_patient_entry("Bob Boyles", 2, 34))
-    db.append(create_patient_entry("Chris Chou", 3, 25))
-    add_test_results(db, 2, "HDL", 100)
-    print(search_db(db, 2))
-    room_list = ["Room 1", "Room 2", "Room 3"]
-    for i, patient in enumerate(db):
-        print(i)
-        print("Name = {}, Room = {}".format(patient[0], room_list[i]))
-    for patient, room in zip(db, room_list):
-        print("Name = {}, Room = {}".format(patient[0], room))
+    db.append(create_patient_entry("Ann", "Ables", 1, 30))
+    db.append(create_patient_entry("Bob", "Boyles", 2, 34))
+    db.append(create_patient_entry("Chris", "Chou", 3, 25))
+    output(db)
+    add_test_results(db, 3, "HDL", 100)
+    output(db)
+    print("Patient {} is a {}".format(get_full_name(db[2]),
+                                      adult_or_minor(db[2])))
 
 
 if __name__ == "__main__":
