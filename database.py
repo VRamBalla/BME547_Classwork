91962eee75e5592e763b77c0bffdb0a9f05af0df
Ramana Balla <vrb12@duke.edu>
2022-09-28 00:41:19 -0400

    made class and init functions and made patient entries with class

--- database.py
+++ database.py
@@ -1,12 +1,24 @@
+class Patient:
+
+    def __init__(self, first_name, last_name, patient_id, age):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.patient_id = patient_id
+        self.age = age
+        self.tests = []
+    
+    
+    def full_name(self):
+        return "{} {}".format(self.first_name, self.last_name)
+
+
 def create_patient_entry(patient_first_name, 
                          patient_last_name,
                          patient_id, 
                          patient_age):
-    new_patient = {"First Name": patient_first_name,
-                   "Last Name": patient_last_name,
-                   "ID": patient_id, 
-                   "Age": patient_age,
-                   "Tests": []}
+    new_patient = Patient(patient_first_name,
+                          patient_last_name, patient_id,
+                          patient_age)
     return new_patient
 
 
@@ -46,6 +58,12 @@ def adult_or_minor(patient):
 
 
 def main():
+    x = Patient("Ramana", "Balla", "", "")
+    x.first_name = "Ramana"
+    x.last_name = "Balla"
+    print(x.full_name())
+    exit()
+    
     db = {}
     db[11] = create_patient_entry("Ann", "Ables", 11, 30)
     db[22] = create_patient_entry("Bob", "Boyles", 22, 34)
